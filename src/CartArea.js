7eecd5b996e56b53223b910fd8703dfd4aede9b9
Arnold Mwangakala <arnoldmwangakala@gmail.com>
2025-01-22 09:01:59 +0300

    Version 1.00

--- src/CartArea.js
+++ src/CartArea.js
@@ -8,13 +8,28 @@ function CartArea({ cart, clearCart }) {
         <div className="cart-area">
             <h1>Cart</h1>
             <div className="list-area">
-                {cartItems.length > 0 ? (<ul className="item-list">
-                    {cartItems.map((item) => (
-                        <li key={item.id}>
-                            {item.name} - ${item.price} x {item.quantity}
-                        </li>
-                    ))}
-                </ul>) : <p id="cart-message">Your cart is empty!</p>}
+                {cartItems.length > 0 ? (<div className="item-list">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Price</th>
+                                <th>Quantity</th>
+                                <th>Total</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {cartItems.map((item) => (
+                                <tr>
+                                    <td>{item.name}</td>
+                                    <td>${item.price}</td>
+                                    <td>{item.quantity}</td>
+                                    <td>${(item.price * item.quantity).toFixed(2)}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>) : <p id="cart-message">Your cart is empty!</p>}
             </div>
             <div className="checkout-area">
                 <p>Total: ${totalPrice.toFixed(2)}</p>
